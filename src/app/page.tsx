9983a120887014729e284abf98b9d147019de9e8
tmdgus95 <8571053@naver.com>
2023-03-25 23:21:18 +0900

    FeaturedPosts 보여주기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Hero from "@/components/Hero";
-import Image from "next/image";
+import FeaturedPosts from '@/components/FeaturedPosts';
+import Hero from '@/components/Hero';
 
 export default function HomePage() {
     return (
         <>
             <Hero />
+            {/* @ts-expect-error Async Server Component */}
+            <FeaturedPosts />
         </>
     );
 }
---
+++ src/components/FeaturedPosts.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostsGrid from './PostsGrid';
+import { getFeaturedPosts } from '@/service/posts';
+
+export default async function FeaturedPosts() {
+    const posts = await getFeaturedPosts();
+    return (
+        <section>
+            <h2 className='text-2xl font-bold'>Featured Posts</h2>
+            <PostsGrid posts={posts} />
+        </section>
+    );
+}
