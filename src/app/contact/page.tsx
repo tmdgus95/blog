4a0cafc1c02f069eaac7179abddc366f4c9a6385
tmdgus95 <8571053@naver.com>
2023-03-25 12:17:03 +0900

    페이지 레이아웃 만들기

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ContactPage() {
+    return <div>연락하기</div>;
+}
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PostsPage() {
+    return <div>포스트들</div>;
+}
