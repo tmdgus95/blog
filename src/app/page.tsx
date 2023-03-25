cce934147484053492d20efad57f9394fb69edfc
tmdgus95 <8571053@naver.com>
2023-03-25 14:05:51 +0900

    프로필 만들기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
-export default function Home() {
-    return <main className="bg-gray-500">Main</main>;
+export default function HomePage() {
+    return (
+        <>
+            <Hero />
+        </>
+    );
 }
