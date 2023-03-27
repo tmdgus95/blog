6e08d1ee294c8b78a080e15e7a58b6009f1fb90b
tmdgus95 <8571053@naver.com>
2023-03-27 21:53:13 +0900

    컴포넌트 분리 리팩토링

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,8 +1,7 @@
-import MarkdownViewer from '@/components/MarkdownViewer';
+import PostContent from '@/components/PostContent';
 import { getPostData } from '@/service/posts';
 import Image from 'next/image';
 import React from 'react';
-import { AiTwotoneCalendar } from 'react-icons/ai';
 
 type Props = {
     params: {
@@ -10,8 +9,8 @@ type Props = {
     };
 };
 export default async function Postpage({ params: { slug } }: Props) {
-    const { title, content, date, description, path } = await getPostData(slug);
-
+    const post = await getPostData(slug);
+    const { title, path } = post;
     return (
         <article className='rounded-2xl overflow-hidden shadow-lg m-4'>
             <Image
@@ -21,17 +20,7 @@ export default async function Postpage({ params: { slug } }: Props) {
                 width={760}
                 height={420}
             />
-
-            <section className='flex flex-col p-4'>
-                <div className='flex items-center self-end text-sky-600'>
-                    <AiTwotoneCalendar />
-                    <p className='font-semibold ml-2'>{date.toString()}</p>
-                </div>
-                <h1 className='text-4xl font-bold'>{title}</h1>
-                <p className='text-xl font-bold'>{description}</p>
-                <div className='w-44 border-2 border-sky-600 mt-4 mb-8 ' />
-                <MarkdownViewer children={content} />
-            </section>
+            <PostContent post={post} />
         </article>
     );
 }
