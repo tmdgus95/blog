a837f6aeef4fe430d55dbf81c2afbf1b1a20cd21
tmdgus95 <8571053@naver.com>
2023-03-28 14:26:19 +0900

    배포를 위한 수정

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,28 +1,28 @@
-'use client';
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import Image from 'next/image';
+"use client";
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import Image from "next/image";
 
 export default function MarkdownViewer({ children }: { children: string }) {
     return (
         <ReactMarkdown
-            className='prose max-w-none'
-            children={children}
+            className="prose max-w-none"
             remarkPlugins={[remarkGfm]}
             components={{
                 code({ node, inline, className, children, ...props }) {
-                    const match = /language-(\w+)/.exec(className || '');
+                    const match = /language-(\w+)/.exec(className || "");
                     return !inline && match ? (
                         <SyntaxHighlighter
-                            children={String(children).replace(/\n$/, '')}
                             language={match[1]}
-                            PreTag='div'
+                            PreTag="div"
                             {...props}
                             style={materialDark}
-                        />
+                        >
+                            {String(children).replace(/\n$/, "")}
+                        </SyntaxHighlighter>
                     ) : (
                         <code className={className} {...props}>
                             {children}
@@ -31,14 +31,16 @@ export default function MarkdownViewer({ children }: { children: string }) {
                 },
                 img: (image) => (
                     <Image
-                        className='w-full max-h-60 object-cover'
-                        src={image.src || ''}
-                        alt={image.alt || ''}
+                        className="w-full max-h-60 object-cover"
+                        src={image.src || ""}
+                        alt={image.alt || ""}
                         width={500}
                         height={500}
                     />
                 ),
             }}
-        />
+        >
+            {children}
+        </ReactMarkdown>
     );
 }
