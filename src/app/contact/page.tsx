f32e91bcf12356fea3353251942dbdb7592aa224
tmdgus95 <8571053@naver.com>
2023-03-28 21:09:40 +0900

    컨텍페이지 레이아웃

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,33 @@
+import ContactForm from "@/components/ContactForm";
 import React from "react";
+import { AiFillGithub } from "react-icons/ai";
 
 export default function ContactPage() {
-    return <div>연락하기</div>;
+    const LINKS = [
+        {
+            icon: <AiFillGithub />,
+            url: "https://github.com/tmdgus95",
+        },
+    ];
+    return (
+        <section className="flex flex-col items-center">
+            <h2 className="text-3xl font-bold my-2">Contact Me</h2>
+            <p>8571053@naver.com</p>
+            <ul className="my-2">
+                {LINKS.map((link, i) => (
+                    <a
+                        key={i}
+                        href={link.url}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="text-5xl hover:text-yellow-400"
+                    >
+                        {link.icon}
+                    </a>
+                ))}
+            </ul>
+            <h2 className="text-3xl font-bold my-8">Or Send me an email</h2>
+            <ContactForm />
+        </section>
+    );
 }
---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,77 @@
+"use client";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import Banner, { BannerData } from "./Banner";
+
+type Form = {
+    from: string;
+    subject: string;
+    message: string;
+};
+export default function ContactForm() {
+    const [form, setForm] = useState<Form>({
+        from: "",
+        subject: "",
+        message: "",
+    });
+    const [banner, setBanner] = useState<BannerData | null>(null);
+    const handleChange = (
+        e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        const { value, name } = e.target;
+        setForm((prev) => ({ ...prev, [name]: value }));
+    };
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setBanner({ message: "성공했어!!", state: "success" });
+        setTimeout(() => {
+            setBanner(null);
+        }, 3000);
+    };
+    return (
+        <section className="w-full max-w-md">
+            {banner && <Banner banner={banner} />}
+            <form
+                onSubmit={handleSubmit}
+                className="w-full  flex flex-col gap-2 my-4 p-4 bg-slate-700 rounded-xl text-white"
+            >
+                <label htmlFor="from" className="font-semibold">
+                    Ypur Email
+                </label>
+                <input
+                    type="email"
+                    id="from"
+                    name="from"
+                    onChange={handleChange}
+                    required
+                    value={form.from}
+                    className="text-black outline-none"
+                />
+                <label htmlFor="subject" className="font-semibold">
+                    제목
+                </label>
+                <textarea
+                    rows={10}
+                    id="subject"
+                    name="subject"
+                    onChange={handleChange}
+                    value={form.subject}
+                    className="text-black outline-none"
+                />
+                <label htmlFor="message" className="font-semibold">
+                    Message
+                </label>
+                <input
+                    type="text"
+                    id="message"
+                    name="message"
+                    onChange={handleChange}
+                    value={form.message}
+                    className="text-black outline-none"
+                />
+                <button className="bg-yellow-300 text-black font-bold hover:bg-yellow-400">
+                    Submit
+                </button>
+            </form>
+        </section>
+    );
+}
