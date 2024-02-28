aa9d8688c23ff1c1626ec3d74c07bc0f9d049221
tmdgus95 <8571053@naver.com>
2024-02-29 03:17:11 +0900

    페이지 레이아웃 만들기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
 import './globals.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const sans = Open_Sans({ subsets: ['latin'] });
 
@@ -15,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={sans.className}>{children}</body>
+    <html lang='en' className={sans.className}>
+      <body className='flex flex-col w-full max-w-screen-2xl mx-auto'>
+        <Header />
+        <main className='grow'>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
