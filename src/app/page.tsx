2e1020201153f8dd2987835a057cea80f7a94505
tmdgus95 <8571053@naver.com>
2024-03-13 01:01:31 +0900

    피쳐된 포스트 보여주기💡- 코드구조

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturedPosts from './components/FeaturedPosts';
 import Hero from './components/Hero';
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <FeaturedPosts />
     </>
   );
 }
